9b077435a41ccf7cb8daefa48e7a5deb1dbcc5d2
Akshay <aks234@hotmail.com>
2019-06-09 22:32:20 -0600

    Update week11.R

--- CodePortfolio/week11.R
+++ CodePortfolio/week11.R
@@ -1,65 +1,44 @@
 #LIBRARY IMPORT
 
-library(tidyverse)
+library(tidyverse)  
+library(cluster)    
+library(factoextra) 
+library(dendextend) 
 
-#READ CSV
+#DATA
 
-heights <- read_csv("C:/Users/aks23/Documents/Harrisburg University/Anly 506/heights.csv")
+df <- USArrests
 
-#INLINE READ
+#SCALE DF
 
-read_csv("a,b,c\n4,5,6", col_names = c("Col 1", "Col 2", "Col 3"))
+df <- scale(df)
 
+head(df)
 
+#EUCLIDEAN DISTANCE
 
-# Defining na values 
-
-read_csv("a,b,c\n4,5,.", na = ".")
-
-#PARSING VECTOR
-
-str(parse_logical(c("TRUE", "FALSE", "NA")))
-
-#PARSING . FOR EMPTY
-
-parse_integer(c("1", "231", ".", "456"), na = ".")
-
-#PARSING NUMBERS
-
-parse_number("$5000")
-
-# PARSING
-
-fruit <- c("apple", "banana")
-
-parse_factor(c("apple", "banana"), levels = fruit)
+d <- dist(df, method = "euclidean")
 
 
-parse_date("2011-01-19")
+
+# Hierarchical CLUSTURING
+
+HCL1 <- hclust(d, method = "complete" )
 
 
-guess_parser("2012-12-11")
+
+# PLOTING
+
+plot(HCL1, cex = 0.6, hang = -1)
 
 
-guess_parser("15:01")
+
+# COMPUTE AGNES
+HCL2 <- agnes(df, method = "complete")
 
 
-#READ_CSV
 
-TEMP <- read_csv(
-  
-  readr_example("TEMP.csv"), 
-  
-  col_types = cols(
-    
-    x = col_integer(),
-    
-    y = col_character()
-    
-  )
+# Agglomerative coefficient
 
-  
-#WRITE CSV
-  
-  write_csv(challenge, "TEMP.csv")
-  
+HCL2$ac
+
