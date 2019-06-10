a35da698fc341ce7080c56fba945ab42a280e0b7
Akshay <aks234@hotmail.com>
2019-06-09 22:23:23 -0600

    Update week9.R

--- CodePortfolio/week9.R
+++ CodePortfolio/week9.R
@@ -1,65 +1,90 @@
-#LIBRARY IMPORT
-
+#IMPORT LIBRARIES
 library(tidyverse)
 
-#READ CSV
+#BARS
 
-heights <- read_csv("C:/Users/aks23/Documents/Harrisburg University/Anly 506/heights.csv")
+ggplot(data = diamonds) +
 
-#INLINE READ
+  geom_bar(mapping = aes(x = cut))
 
-read_csv("a,b,c\n4,5,6", col_names = c("Col 1", "Col 2", "Col 3"))
+#NUMBER OF OBSERVATIONS
+diamonds %>% count(cut)
+
+#HISTOGRAM
+
+ggplot(data = diamonds) +
+
+  geom_histogram(mapping = aes(x = carat), binwidth = 0.5)
+
+#FILTER
+smaller <- diamonds %>% 
+
+  filter(carat < 3)
+
+#HISTOGRAMS LINES
+ggplot(data = smaller, mapping = aes(x = carat, colour = cut)) +
+
+  geom_freqpoly(binwidth = 0.1)
+
+#BINWIDTH
+ggplot(diamonds) + 
+
+  geom_histogram(mapping = aes(x = y), binwidth = 0.5)
+
+ggplot(data = faithful, mapping = aes(x = eruptions)) + 
+
+  geom_histogram(binwidth = 0.25)
+
+#NEW VARIABLE
+nycflights13::flights %>% 
+
+  mutate(
+
+    cancelled = is.na(dep_time),
+
+    ed_hour = sched_dep_time %/% 100,
+
+    sched_min = sched_dep_time %% 100,
+
+    sched_dep_time = sched_hour + sched_min / 60
+
+  ) %>% 
+
+#SHOWING NEW VARIABLE
+ggplot(mapping = aes(sched_dep_time)) + 
+
+  geom_freqpoly(mapping = aes(colour = cancelled), binwidth = 1/4)
+
+# 
+
+ggplot(data = mpg) +
+
+  geom_boxplot(mapping = aes(x = reorder(class, hwy, FUN = median), y = hwy))
 
 
 
-# Defining na values 
+diamonds %>% 
 
-read_csv("a,b,c\n4,5,.", na = ".")
-
-#PARSING VECTOR
-
-str(parse_logical(c("TRUE", "FALSE", "NA")))
-
-#PARSING . FOR EMPTY
-
-parse_integer(c("1", "231", ".", "456"), na = ".")
-
-#PARSING NUMBERS
-
-parse_number("$5000")
-
-# PARSING
-
-fruit <- c("apple", "banana")
-
-parse_factor(c("apple", "banana"), levels = fruit)
+  count(color, cut)
 
 
-parse_date("2011-01-19")
+
+# use a model to remove the very strong relationship between price and carat so we can explore the subtleties that remain
+
+library(modelr)
+
+mod <- lm(log(price) ~ log(carat), data = diamonds)
 
 
-guess_parser("2012-12-11")
+
+diamonds2 <- diamonds %>% 
+
+  add_residuals(mod) %>% 
+
+  mutate(resid = exp(resid))
 
 
-guess_parser("15:01")
 
+ggplot(data = diamonds2) + 
 
-#READ_CSV
-
-TEMP <- read_csv(
-  
-  readr_example("TEMP.csv"), 
-  
-  col_types = cols(
-    
-    x = col_integer(),
-    
-    y = col_character()
-    
-  )
-
-  
-#WRITE CSV
-  
-  write_csv(challenge, "TEMP.csv")
-  
+  geom_point(mapping = aes(x = carat, y = resid))
